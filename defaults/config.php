c4565c72123fc990a82377dcc04861c803151db0
banyazavi <facelaw04@gmail.com>
2019-09-04 18:54:00 +0900

    Update default config.php for showdown-manager-1.0.1

--- defaults/config.php
+++ defaults/config.php
@@ -15,15 +15,20 @@
     // Linux 기반사 용자의 경우 showdown의 실행 권한을 showdown-manager web 실행 권한(ex. http)과 showdown 파일들의 소유자를 같이 맞출 필요가 있음
     $show_log_menu = 'Y';
 
+    // Web UI Theme 선택.
+    $manager_theme = 'black'; // default , black
+
     ## 이 이하는 수정하지 마세요.
     $showdown_min_ver = 'v1.54'; // Showdown 필요 버전
-    $showdown_manager_ver = 'v1.0.0'; // Showdown Manager 버전
+    $showdown_manager_ver = 'v1.0.1'; // Showdown Manager 버전
 
     ## Docker용 환경 설정
     if ( trim($_SERVER['WEB_DOCUMENT_ROOT']) != '' ) $http_path = trim($_SERVER['WEB_DOCUMENT_ROOT']);
     if ( trim($_SERVER['SHOWDOWN_PATH']) != '') $client_path = trim($_SERVER['SHOWDOWN_PATH']);
     if ( trim($_SERVER['SHOWDOWN_START_PAGE']) != '' ) $start_page = trim($_SERVER['SHOWDOWN_START_PAGE']);
     if ( trim($_SERVER['SHOWDOWN_LOG_VIEW']) != '' ) $show_log_menu = trim($_SERVER['SHOWDOWN_LOG_VIEW']);
+
+    if ( trim($_SERVER['SHOWDOWN_MANAGER_THEME']) != '' ) $manager_theme = trim($_SERVER['SHOWDOWN_MANAGER_THEME']);
     if ( trim($_SERVER['SHOWDOWN_MANAGER_ID']) != '' ) $manager_id = trim($_SERVER['SHOWDOWN_MANAGER_ID']);
     if ( trim($_SERVER['SHOWDOWN_MANAGER_PASSWD']) != '' ) $manager_passwd = trim($_SERVER['SHOWDOWN_MANAGER_PASSWD']);
 
